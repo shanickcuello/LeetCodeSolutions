ef2176d9f618e02aeb692845a757560d5124be9d
shanickcuello <shanlogauthier@gmail.com>
2024-01-03 21:32:48 +0100

    Day3 LengthOfLongestSubstring solution

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -4,16 +4,9 @@ using LeetCodeSolutions.Scripts;
 
 Console.WriteLine("Hello, World!");
 
+var result = LengthOfLongestSubstringSolution.LengthOfLongestSubstring("pwwkew");
+var result2 = LengthOfLongestSubstringSolution.LengthOfLongestSubstring("abcabcbb");
+var result3 = LengthOfLongestSubstringSolution.LengthOfLongestSubstring("bbbbb");
 
-ListNode listNode1 = new ListNode(2,
-    new ListNode(4, new ListNode(3)));
-
-ListNode listNode2 = new ListNode(5,
-    new ListNode(6, new ListNode(4)));
-
-AddTwoNumbersSolution addTwoNumbersSolution = new AddTwoNumbersSolution();
-
-var result = addTwoNumbersSolution.AddTwoNumbers(listNode1, listNode2);
-
-Console.WriteLine(result);
+Console.WriteLine(result + result2.ToString() + result3);
 Console.ReadLine();
---
+++ LeetCodeSolutions/Scripts/LengthOfLongestSubstringSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutions.Scripts;
+
+public static class LengthOfLongestSubstringSolution
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> amountOfCharsPerSubstring = new Dictionary<char, int>();
+        int startIndexOfSubstrings = 0;
+        int maxAmountOfCharsFromSubstringWithoutRepeating = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var charEvalated = s[i];
+            if (amountOfCharsPerSubstring.ContainsKey(charEvalated) && amountOfCharsPerSubstring[charEvalated] >= startIndexOfSubstrings)
+            {
+                startIndexOfSubstrings = amountOfCharsPerSubstring[charEvalated] + 1;
+            }
+
+            amountOfCharsPerSubstring[charEvalated] = i;
+            int currentLength = i - startIndexOfSubstrings + 1;
+
+            maxAmountOfCharsFromSubstringWithoutRepeating = Math.Max(maxAmountOfCharsFromSubstringWithoutRepeating, currentLength);
+        }
+
+        return maxAmountOfCharsFromSubstringWithoutRepeating;
+    }
+}
