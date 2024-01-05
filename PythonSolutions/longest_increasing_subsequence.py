2dbfa171c6c4c5c84d8210741b15d1057e5ed315
shanickcuello <shanlogauthier@gmail.com>
2024-01-05 19:43:10 +0100

    Day 5 Longest Increasing Subsequence

--- PythonSolutions/longest_increasing_subsequence.py
+++ PythonSolutions/longest_increasing_subsequence.py
@@ -3,3 +3,12 @@ from typing import List
 
 class LongestIncreasingSubsequence:
     def lengthOfLIS(nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+if __name__ == '__main__':
+    LongestIncreasingSubsequence.lengthOfLIS([10,9,2,5,3,7,101,18])
