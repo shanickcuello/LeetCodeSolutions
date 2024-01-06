2fd5551b1248e1229d020146570578e6c598a88a
shanickcuello <shanlogauthier@gmail.com>
2024-01-06 22:29:14 +0100

    Add more cases and optimize algorithm

--- PythonSolutions/maximum_profit_in_job_scheduling.py
+++ PythonSolutions/maximum_profit_in_job_scheduling.py
@@ -1,21 +1,22 @@
+from _bisect import bisect_right
 from typing import List
 
 
 def jobScheduling(startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-    jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-    n = len(jobs)
-    dp = [0] * n
+    jobs = sorted(zip(endTime, startTime, profit))
 
-    for i in range(n):
-        dp[i] = jobs[i][2]  # Initialize with the profit of the current job
+    number_of_jobs = len(profit)
 
-    for i in range(1, n):
-        for j in range(i):
-            if jobs[i][0] >= jobs[j][1]:
-                dp[i] = max(dp[i], dp[j] + jobs[i][2])
+    dp = [0] * (number_of_jobs + 1)
 
-    return max(dp)
+    for i, (current_end_time, current_start_time, current_profit) in enumerate(jobs):
+        index = bisect_right(jobs, current_start_time, hi=i, key=lambda x: x[0])
+        dp[i + 1] = max(dp[i], dp[index] + current_profit)
+
+    print(dp[number_of_jobs])
+    return dp[number_of_jobs]
 
 if __name__ == '__main__':
-    result = jobScheduling([1,2,3,3], [3,4,5,6], [50,10,40,70])
-    print(result)
+    jobScheduling([1,2,3,3], [3,4,5,6], [50,10,40,70])
+    jobScheduling([1,2,3,4,6], [3,5,10,6,9], [20,20,100,70,60])
+    jobScheduling([1,1,1], [2,3,4], [5,6,4])
