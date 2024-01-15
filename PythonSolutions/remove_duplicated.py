2ed13f9e7932f00107f5bf78836aad698ac3b497
shanickcuello <shanlogauthier@gmail.com>
2024-01-15 22:44:19 +0100

    Day 15 remove duplicated

---
+++ PythonSolutions/remove_duplicated.py
@@ -0,0 +1,29 @@
+from typing import List
+
+# # this solution lead me to runtime problems modifying the list while iterating.
+# def remove_duplicates(nums: List[int]) -> int:
+#     current_num = None
+#
+#     for i in range(len(nums)):
+#         if current_num == nums[i]:
+#             nums[i] = None
+#         elif current_num != nums[i]:
+#             current_num = nums[i]
+#     return len([num for num in nums if num is not None])
+
+def remove_duplicates(nums: List[int]) -> int:
+    current_num = None
+    non_duplicates = []
+
+    for num in nums:
+        if current_num != num:
+            current_num = num
+            non_duplicates.append(num)
+
+    nums[:] = non_duplicates
+    return len(nums)
+
+
+if __name__ == '__main__':
+    print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+    print(remove_duplicates([1, 1, 2]))
