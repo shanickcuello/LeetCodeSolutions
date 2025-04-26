7122595c49275948205ee9765f19cf032a0d9894
vidar <shanlogauthier@gmail.com>
2025-04-27 00:07:21 +0200

    Add ThreeSum solution

---
+++ C#Solutions/LeetCodeSolutions/Scripts/ThreeSum.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+        IList<IList<int>> result = new List<IList<int>>();
+
+        for(var i = 0; i < nums.Length - 2; i++)
+        {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            while(left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0)
+                {
+                    result.Add(new List<int> {nums[i], nums[left], nums[right]});
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }else if(sum < 0)
+                    left++;
+                else
+                    right--;
+                
+            } 
+        }
+        return result;
+    }
+}
